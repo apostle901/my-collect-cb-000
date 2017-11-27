0e19a21c5f624aee78462a68f2b865457ada0e54
Jawann <jawann@gmail.com>
2017-11-27 03:53:02 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect (array)
+  if block_given?
+    idx = 0
+    collection = []
+    while idx < array.length do
+      collection << yield(array[idx])
+      idx += 1
+    end
+    collection
+end
 
